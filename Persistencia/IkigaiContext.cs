15d1dfa2364e0816db15324c7d4a6a57798a2cc1
joseedet <joseedet@gmail.com>
2024-01-02 12:05:04 +0100

    Modificacion IkigaiContext

--- Persistencia/IkigaiContext.cs
+++ Persistencia/IkigaiContext.cs
@@ -18,82 +18,13 @@ namespace Persistencia
         
 
 
-<<<<<<< HEAD
+
             public DbSet<Planin> Agendas {get;set;}
             public DbSet<Cliente> Clientes {get;set;}
             public DbSet<DetalleCliente> DetalleClientes {get;set;}
             public DbSet<Tipo> Tipos {get;set;}
-            
 
-        
-=======
-            public DbSet<Planin> Agendas => Set <Planin>();
-            public DbSet<Cliente> Clientes => Set <Cliente>();
-            public DbSet<DetalleCliente> DetalleClientes => Set <DetalleCliente>();
-            public DbSet<Tipo> Tipos => Set <Tipo>();
-            
-
-        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
-            {
-                var result = await base.SaveChangesAsync(cancellationToken);
-
-                return result;
-            }
-
-        private IDbContextTransaction _currentTransaction;
-        public IDbContextTransaction GetCurrentTransaction() => _currentTransaction;
-        public bool HasActiveTransaction => throw new NotImplementedException();
-
-        public async Task<IDbContextTransaction> BeginTransactionAsync()
-        {
-             if (_currentTransaction != null) return null!;
-
-            _currentTransaction = await Database.BeginTransactionAsync();
-
-            return _currentTransaction;
-        }
-
-        public async Task CommitAsync(IDbContextTransaction transaction)
-        {
-            if (transaction == null) throw new ArgumentNullException(nameof(transaction));
-            if (transaction != _currentTransaction) throw new InvalidOperationException($"Transaction {transaction.TransactionId} is not current");
-
-            try
-            {
-                await SaveChangesAsync();
-                transaction.Commit();
-            }
-            catch
-            {
-                RollbackTransaction();
-                throw;
-            }
-            finally
-            {
-                if (_currentTransaction != null)
-                {
-                    _currentTransaction.Dispose();
-                    _currentTransaction = null!;
-                }
-            }
-        }
-
-        private void RollbackTransaction()
-        {
-                try
-            {
-                _currentTransaction.Rollback();
-            }
-            finally
-            {
-                if (_currentTransaction != null)
-                {
-                    _currentTransaction.Dispose();
-                    _currentTransaction = null!;
-                }
-            }
-        }
-
->>>>>>> 5971c0778af140f4de860fccacfa5606f14e609e
-    }  
+    }
 }
+            
+
